1d8ee15290000881cdbdfb731266d588f8d7726e
790845 <LB790845@slfl1740.selfridges.root.local>
2022-06-22 11:04:27 +0100

    dev

--- src/dev/config.ts
+++ src/dev/config.ts
@@ -59,97 +59,170 @@ export const quizValues: QuizValues = {
       },
     ],
   },
-  // 2: {
-  //   img: question_2,
-  //   question: "What is your favorite coffee shop?",
-  //   options: [
-  //     {
-  //       option: "starbucks or costa",
-  //       point: 3,
-  //       keyWords: ["coffee", 'Cafe',"BALENCIAGA", chocolate", "fragrance"],
-  //     },
-  //     {
-  //       option: "small local cafe",
-  //       point: 1,
-  //       keyWords: ["fragrance",'Cafe', "funny",'Blossom', "sweatshirt", "hoodie", 'BEAUTY'],
-  //     },
-  //     {
-  //       option: "No coffee, only cuppa",
-  //       point: 2,
-  //       keyWords: ["tea",   "Classic",
-  // "Classico",
-  // "Classics","jam", "rose", 'COFFEE','Blend','Book' ],
-  //     },
-  //   ],
-  // },
-  // 3: {
-  //   img: question_3,
-  //   question: "What is one TV series you can watch over and over again?",
-  //   options: [
-  //     {
-  //       option: "Game of Thrones",
-  //       point: 3,
-  //       keyWords: ["Game of Thrones", "ANCIENT", "box", "rome","games", "mindfulness", "Bio-Synergy"],
-  //     },
-  //     {
-  //       option: "Peppa pig forever",
-  //       point: 1,
-  //       keyWords: ["RAINBOW", "kids", 'Sharks', 'Smiley', 'Snapshot', 'Sparkle', 'Summer'],
-  //     },
-  //     {
-  //       option: "Peaky Blinders",
-  //       point: 2,
-  //       keyWords: ["BOTTLE OPENER",'BBQ', 'Ritual' ,"DR. MARTENS", "black bag"],
-  //     },
-  //   ],
-  // },
-  // 4: {
-  //   img: question_4,
-  //   question: "They prefer...",
-  //   options: [
-  //     {
-  //       option: "carrot juice",
-  //       point: 3,
-  //       keyWords: ["earth", 'eco','Caramels', "fruit", "fresh", "rainbow"],
-  //     },
-  //     {
-  //       option: "beer",
-  //       point: 1,
-  //       keyWords: ["beer", 'Cider', "gentlemen", "lazy", "gaming"],
-  //     },
-  //     {
-  //       option: "wine",
-  //       point: 2,
-  //       keyWords: ["aesthetic",'Croissant','DESIGN', 'Classics','Cognac', "design", "satin"],
-  //     },
-  //   ],
-  // },
-  // 5: {
-  //   img: question_5,
-  //   question: "This person loves...",
-  //   options: [
-  //     {
-  //       option: "modern tech",
-  //       point: 3,
-  //       keyWords: ["apple", 'RESEARCH', 'RAY-BAN','Projector', "the tech bar", 'Cosmic', "controller", "binoculars"],
-  //     },
-  //     {
-  //       option: "sustainable fashion",
-  //       point: 1,
-  //       keyWords: ["sustainable", "ecology", "planet", "bundle", "set",   "Compact",
-  // "Companion",
-  // "Company",
-  // "Compass",
-  // "Compostable",
-  // "Compound",],
-  //     },
-  //     {
-  //       option: "dinner with kids",
-  //       point: 2,
-  //       keyWords: ['Pokemon',  "cozy","BARBIE",'Prince', "set", 'DISCOVERY', 'disney', "cozy", 'DOLLS','Cream'],
-  //     },
-  //   ],
-  // },
+  2: {
+    img: question_2,
+    question: "What is your favorite coffee shop?",
+    options: [
+      {
+        option: "starbucks or costa",
+        point: 3,
+        keyWords: ["coffee", "Cafe", "BALENCIAGA", "chocolate", "fragrance"],
+      },
+      {
+        option: "small local cafe",
+        point: 1,
+        keyWords: [
+          "fragrance",
+          "Cafe",
+          "funny",
+          "Blossom",
+          "sweatshirt",
+          "hoodie",
+          "BEAUTY",
+        ],
+      },
+      {
+        option: "No coffee, only cuppa",
+        point: 2,
+        keyWords: [
+          "tea",
+          "Classic",
+          "Classico",
+          "Classics",
+          "jam",
+          "rose",
+          "COFFEE",
+          "Blend",
+          "Book",
+        ],
+      },
+    ],
+  },
+  3: {
+    img: question_3,
+    question: "What is one TV series you can watch over and over again?",
+    options: [
+      {
+        option: "Game of Thrones",
+        point: 3,
+        keyWords: [
+          "Game of Thrones",
+          "ANCIENT",
+          "box",
+          "rome",
+          "games",
+          "mindfulness",
+          "Bio-Synergy",
+        ],
+      },
+      {
+        option: "Peppa pig forever",
+        point: 1,
+        keyWords: [
+          "RAINBOW",
+          "kids",
+          "Sharks",
+          "Smiley",
+          "Snapshot",
+          "Sparkle",
+          "Summer",
+        ],
+      },
+      {
+        option: "Peaky Blinders",
+        point: 2,
+        keyWords: [
+          "BOTTLE OPENER",
+          "BBQ",
+          "Ritual",
+          "DR. MARTENS",
+          "black bag",
+        ],
+      },
+    ],
+  },
+  4: {
+    img: question_4,
+    question: "They prefer...",
+    options: [
+      {
+        option: "carrot juice",
+        point: 3,
+        keyWords: ["earth", "eco", "Caramels", "fruit", "fresh", "rainbow"],
+      },
+      {
+        option: "beer",
+        point: 1,
+        keyWords: ["beer", "Cider", "gentlemen", "lazy", "gaming"],
+      },
+      {
+        option: "wine",
+        point: 2,
+        keyWords: [
+          "aesthetic",
+          "Croissant",
+          "DESIGN",
+          "Classics",
+          "Cognac",
+          "design",
+          "satin",
+        ],
+      },
+    ],
+  },
+  5: {
+    img: question_5,
+    question: "This person loves...",
+    options: [
+      {
+        option: "modern tech",
+        point: 3,
+        keyWords: [
+          "apple",
+          "RESEARCH",
+          "RAY-BAN",
+          "Projector",
+          "the tech bar",
+          "Cosmic",
+          "controller",
+          "binoculars",
+        ],
+      },
+      {
+        option: "sustainable fashion",
+        point: 1,
+        keyWords: [
+          "sustainable",
+          "ecology",
+          "planet",
+          "bundle",
+          "set",
+          "Compact",
+          "Companion",
+          "Company",
+          "Compass",
+          "Compostable",
+          "Compound",
+        ],
+      },
+      {
+        option: "dinner with kids",
+        point: 2,
+        keyWords: [
+          "Pokemon",
+          "cozy",
+          "BARBIE",
+          "Prince",
+          "set",
+          "DISCOVERY",
+          "disney",
+          "cozy",
+          "DOLLS",
+          "Cream",
+        ],
+      },
+    ],
+  },
 };
 
 export const configurations: Config = {
